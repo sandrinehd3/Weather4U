371672c4386a631f2198a359f06b4734d783fb86
sandrinehd3 <65869203+sandrinehd3@users.noreply.github.com>
2020-07-30 18:21:44 -0400

    Updated Temperature Elements
    Current temp, day's high and low temps, weather description

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#temp-description");
+  let highElement = document.querySelector("#hightemp");
+  let lowElement = document.querySelector("#lowtemp");
+  let sunriseElement = document.querySelector("#sunrise");
+  let sunsetElement = document.querySelector("#sunset");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  highElement.innerHTML = Math.round(response.data.main.temp_max);
+  lowElement.innerHTML = Math.round(response.data.main.temp_min);
+  sunriseElement.innerHTML = response.data.sys.sunrise;
+  sunsetElement.innerHTML = response.data.sys.sunset;
 }
 
 let apiKey = "bdd263b59088bdadbee5570c95e7c44e";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&APPID=${apiKey}&units=imperial`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&APPID=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
