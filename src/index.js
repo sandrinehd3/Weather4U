8bb1212bb9bef707a2f7ed3cc1a6f6191d9a9d0a
sandrinehd3 <65869203+sandrinehd3@users.noreply.github.com>
2020-07-30 16:41:10 -0400

    API  and Axios applied
    API key and url tested via temperature.

--- src/index.js
+++ src/index.js
@@ -1,42 +1,10 @@
-let now = new Date();
-let h2 = document.querySelector("h2");
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-
-let day = days[now.getDay()];
-let month = months[now.getMonth()];
-let date = now.getDate();
-let year = now.getFullYear();
-let hour = now.getHours();
-if (hour < 10) {
-  hour = `0${hour}`;
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
 
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
+let apiKey = "bdd263b59088bdadbee5570c95e7c44e";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&APPID=${apiKey}&units=imperial`;
 
-h2.innerHTML = `${day},  ${month} ${date}, ${year}  <br> ${hour}:${minutes}`;
+axios.get(apiUrl).then(displayTemperature);
